4c6a4d551ec38c9415bbd5cada825773cbdf5e8c
novartyeka <novarty@gmail.com>
2026-09-02 18:28:26 +0700

    Delete seo-content-studio directory

--- seo-content-studio/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SEOContentStudioPro from './seo_content_studio_pro.tsx'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <SEOContentStudioPro />
-  </React.StrictMode>,
-)
